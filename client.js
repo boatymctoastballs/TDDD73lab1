0b6a9c55b4904cf8209266b525346f6e0d8f6a33
Tor Utterborn <torut235@li1-10.ida.liu.se>
2017-01-26 20:46:14 +0100

    nu ska vi gora andra css pa header, det var en katt som vi ville klappa

--- client.js
+++ client.js
@@ -1,26 +1,113 @@
-	displayView = function(view){
-	   // the code required to display a view
-	   document.getElementById("clientView").innerHTML = view;
-	};
-window.onload = function(){
-   //code that is executed as the page is loaded.
-   //You shall put your own custom code here.
-   	const welcomeView = document.getElementById('welcomeView');
-   	const profileView = document.getElementById('profileView');
-
-   	let currentView = document.getElementById('clientView');
-
-   	var loginButton = document.getElementById('loginButton');
-   	var signupButton = document.getElementById('signupButton'); 
-
-
-	if(currentView == ""){
-		displayView(welcomeView.innerHTML);
-	}
-	else if(loginButton.onclick = function() || signupButton.onclick = function()) {
-		displayView(profileView.innerHTML);
-	}
-
-//window.alert() is not allowed to be used in your implementation.
-    window.alert("Hello TDDD97!");
+displayView = function(view){
+   // the code required to display a view
+   clientviewer = document.getElementById('clientviewer').innerHTML;
+   document.getElementById("clientviewer").innerHTML = view;
 };
+
+window.onload = function(){
+    welcomeview  = document.getElementById('welcomeview').innerHTML;
+
+    profileview  = document.getElementById('profileview').innerHTML;
+    displayView(profileview);
+
+    document.getElementById('signup').onsubmit=signupChecker;
+    document.getElementById('login').onsubmit=signInChecker;// sa att sidan inte laddas om
+//code that is executed as the page is loaded.
+//You shall put your own custom code here.
+//window.alert() is not allowed to be used in your implementation.
+};
+
+signupChecker = function(e){    welcomeview  = document.getElementById('welcomeview').innerHTML;
+    welcomeview  = document.getElementById('welcomeview').innerHTML;
+
+  e.preventDefault();
+  minpwlenght = 3;
+  var pass1 = document.getElementById('signup-password').value;
+  var pass2 = document.getElementById('signup-password-RE').value;
+  var PWchecker = true;
+  if (pass1 != pass2){
+    alert("Stop this! Password does not match")
+    PWchecker = false;
+  }
+  else if (pass1.length<minpwlenght || pass2.length<minpwlenght) {
+    alert("Stop tihs! Password to short")
+    PWchecker = false;
+  }
+
+  if(PWchecker){
+    alert("OK")
+  }
+
+
+  var object = {
+
+    email : document.getElementById('emailInput').value,
+    password : document.getElementById('signup-password').value,
+    firstname : document.getElementById('firstnameInput').value,
+    familyname : document.getElementById('familynameInput').value,
+    gender : document.getElementById('genderInput').value,
+    city : document.getElementById('cityInput').value,
+    country : document.getElementById('countryInput').value
+
+  };
+  console.log(object);
+  var check = serverstub.signUp(object);
+  console.log(check);
+
+  return PWchecker
+}
+
+
+//validate email
+signInChecker = function(e){
+  e.preventDefault();
+//decide view
+  profileview  = document.getElementById('profileview').innerHTML;
+  welcomeview  = document.getElementById('welcomeview').innerHTML;
+  if(validateSignin().success){
+    displayView(profileview)
+  }
+  else{
+    displayView(welcomeview)
+  }
+
+}
+
+validateSignin = function(){
+  var username = document.getElementById('loginUsername').value;
+  var password = document.getElementById('loginPassword').value;
+  var check = serverstub.signIn(username, password);
+  console.log(check);
+  return check
+}
+
+headerfunc = function(header){
+  var headerHome = getElementById("headerHome").value;
+  var headerBrowse = getElementById("headerBrowse").value;
+  var headerAccount = getElementById("headerAccount").value;
+
+  switch (header) {
+    case headerHome:
+      document.getElementById('headerHome').style.display = 'block'
+      document.getElementById('headerBrowse').style.display = 'none'
+      document.getElementById('headerAccount').style.display = 'none'
+      return;
+    case headerBrowse:
+      document.getElementById('headerHome').style.display = 'none'
+      document.getElementById('headerBrowse').style.display = 'block'
+      document.getElementById('headerAccount').style.display = 'none'
+      return;
+    case headerAccount:
+      document.getElementById('headerHome').style.display = 'none'
+      document.getElementById('headerBrowse').style.display = 'none'
+      document.getElementById('headerAccount').style.display = 'block'
+      return;
+    default:
+    document.getElementById('headerHome').style.display = 'none'
+    document.getElementById('headerBrowse').style.display = 'none'
+    document.getElementById('headerAccount').style.display = 'none'
+  }
+  document.getElementById('id1').style.color =
+  document.getElementById('id1').style.color = 'red'
+  document.getElementById('id1').style.color = 'red'
+}
